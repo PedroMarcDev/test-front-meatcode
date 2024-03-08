b06f125339dd8a9a04a42de2c5162753a14259fb
Pedro Marcano <pedromarcdev@gmail.com>
2024-03-08 13:03:34 -0400

    feat: create hero

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import Hero from './components/Hero.vue';
+
+export default {
+  components: {
+    Hero
+  },
+  props: {
+    campaign: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  watch: {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+
 </script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<template>
+  <Hero />
+</template>
---
+++ src/components/Hero.vue
@@ -0,0 +1,47 @@
+<script>
+    export default {
+        props: {
+            image: {
+                type: String,
+                required: true
+            },
+
+            title: {
+                type: String,
+                required: true
+            }
+        },
+        data() {
+
+        },
+        watch: {
+            
+        },
+        mounted() {
+            
+        },
+        methods: {
+           test() {
+            
+           },
+        }
+    }
+</script>
+
+
+<template>
+    <div class="hero">
+        <div class="hero__background">
+            <img src="../assets/img/hero_BG.png" class="hero__image" alt="Hero background">
+        </div>
+
+        <div class="hero__title-container">
+            <h1 class="hero__title">
+                El secreto de tu cocina
+            </h1>
+        </div>
+
+        <div class="hero__separator">
+        </div>
+    </div>
+</template>
